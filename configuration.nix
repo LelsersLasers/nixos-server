93390c9a7be0ffaddb16aca9e0630ceef29b7a5c
Millan Kumar (nixos server) <millankumar@gmail.com>
2025-11-25 14:27:34 -0800

    Switching to unstable

--- configuration.nix
+++ configuration.nix
@@ -4,17 +4,10 @@
 
 { config, pkgs, ... }:
 
-let
-  home-manager = builtins.fetchTarball {
-    url = https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
-    sha256 = "026rvynmzmpigax9f8gy9z67lsl6dhzv2p6s8wz4w06v3gjvspm1";
-  };
-in
 {
   imports =
     [ 
       ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   description = "Personal NixOS server configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;  
     in
@@ -13,6 +15,7 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.default
         ];
       };
     };
